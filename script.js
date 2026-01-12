2f06de15d851eb9128c52e1f1ea183fee5552003
Stanley Mazile <stanleymazilepro@gmail.com>
2026-01-12 17:08:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const btnMenu = document.getElementById('btnMenu');
 const menu = document.getElementById('liens-deroulants');
 
+// Ouverture/Fermeture du menu au clic sur le bouton
 btnMenu.onclick = function(e) {
     menu.classList.toggle('voir');
     e.stopPropagation();
 }
 
+// Fermeture automatique si on clique n'importe où ailleurs sur la page
 window.onclick = function() {
-    menu.classList.remove('voir');
+    if (menu.classList.contains('voir')) {
+        menu.classList.remove('voir');
+    }
 }
