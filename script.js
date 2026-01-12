9ce84b59043ad8b9e82df701b520d2a365b0a236
Stanley Mazile <stanleymazilepro@gmail.com>
2026-01-12 18:13:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,22 +3,22 @@ const menuPrincipal = document.getElementById('liens-deroulants');
 const btnCompte = document.getElementById('btnCompte');
 const menuCompte = document.getElementById('menu-compte');
 
-// Gestion Menu Principal
-btnMenu.onclick = function(e) {
+// Menu Principal
+btnMenu.onclick = (e) => {
     menuPrincipal.classList.toggle('voir');
-    menuCompte.classList.remove('voir'); // Ferme l'autre
+    menuCompte.classList.remove('voir');
     e.stopPropagation();
-}
+};
 
-// Gestion Menu Compte
-btnCompte.onclick = function(e) {
+// Menu Compte
+btnCompte.onclick = (e) => {
     menuCompte.classList.toggle('voir');
-    menuPrincipal.classList.remove('voir'); // Ferme l'autre
+    menuPrincipal.classList.remove('voir');
     e.stopPropagation();
-}
+};
 
-// Clic n'importe où pour fermer
-window.onclick = function() {
+// Fermeture au clic extérieur
+window.onclick = () => {
     menuPrincipal.classList.remove('voir');
     menuCompte.classList.remove('voir');
-}
+};
