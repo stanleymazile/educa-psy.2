6944320fb35b17a040c90c827eda774a159a257b
Stanley Mazile <stanleymazilepro@gmail.com>
2026-01-13 03:43:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,25 @@ const btnMenu = document.getElementById('btnMenu');
 const menuPrincipal = document.getElementById('liens-deroulants');
 const barres = document.querySelectorAll('.barre');
 
-// Toggle Menu avec animation de l'icône
 btnMenu.addEventListener('click', (e) => {
     menuPrincipal.classList.toggle('voir');
     
+    // Animation de l'icône à 3 barres
     if(menuPrincipal.classList.contains('voir')) {
-        barres[0].style.transform = "translateY(2.5px) rotate(45deg)";
-        barres[1].style.transform = "translateY(-2.5px) rotate(-45deg)";
+        barres[0].style.transform = "translateY(5.5px) rotate(45deg)";
+        barres[1].style.opacity = "0";
+        barres[2].style.transform = "translateY(-5.5px) rotate(-45deg)";
     } else {
         barres[0].style.transform = "none";
-        barres[1].style.transform = "none";
+        barres[1].style.opacity = "1";
+        barres[2].style.transform = "none";
     }
     e.stopPropagation();
 });
 
-// Fermer le menu si on clique ailleurs
 document.addEventListener('click', () => {
     menuPrincipal.classList.remove('voir');
-    barres.forEach(b => b.style.transform = "none");
+    barres[0].style.transform = "none";
+    barres[1].style.opacity = "1";
+    barres[2].style.transform = "none";
 });
