c87c3f5c820d8fe3401c3ec0003d88bf0e8f9818
Stanley Mazile <stanleymazilepro@gmail.com>
2026-01-13 13:23:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-## **script.js**
-
-```javascript
 const btnMenu = document.getElementById('btnMenu');
 const menu = document.getElementById('liens-deroulants');
 
+// Ouvrir/fermer le menu
 btnMenu.onclick = function(e) {
     menu.classList.toggle('voir');
     e.stopPropagation();
-}
+};
 
-window.onclick = function() {
+// Fermer le menu si clic en dehors
+document.addEventListener('click', function() {
     menu.classList.remove('voir');
-}
+});
 
+// Gestion du bouton langue (sans Google Translate)
 document.getElementById('select-langue').addEventListener('change', function() {
     var lang = this.value;
-    console.log('Langue sélectionnée:', lang);
+    console.log("Langue choisie :", lang);
+    // Exemple : redirection vers une version traduite
+    // window.location.href = lang + "/index.html";
 });
-```
