8eca1072650e3345bc4ed9c030a903807e1d5ba0
Stanley Mazile <stanleymazilepro@gmail.com>
2026-01-13 15:45:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,21 +3,18 @@ const nav = document.getElementById('liens-deroulants');
 const btnLangue = document.getElementById('btnLangue');
 const menuLangue = document.getElementById('menuLangue');
 
-// Toggle Menu Burger
 btnMenu.onclick = (e) => {
     nav.classList.toggle('voir');
     menuLangue.classList.remove('active');
     e.stopPropagation();
 };
 
-// Toggle Langue
 btnLangue.onclick = (e) => {
     menuLangue.classList.toggle('active');
     nav.classList.remove('voir');
     e.stopPropagation();
 };
 
-// Fermeture au clic extérieur
 window.onclick = () => {
     nav.classList.remove('voir');
     menuLangue.classList.remove('active');
