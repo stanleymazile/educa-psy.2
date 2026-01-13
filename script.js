62309f5089869f13b12368bcc559455beba07018
Stanley Mazile <stanleymazilepro@gmail.com>
2026-01-13 03:08:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ const btnMenu = document.getElementById('btnMenu');
 const menuPrincipal = document.getElementById('liens-deroulants');
 const barres = document.querySelectorAll('.barre');
 
-// Toggle Menu avec animation Hamburger
 btnMenu.addEventListener('click', (e) => {
     menuPrincipal.classList.toggle('voir');
-    
     if(menuPrincipal.classList.contains('voir')) {
         barres[0].style.transform = "rotate(45deg) translateY(4px)";
         barres[1].style.transform = "rotate(-45deg) translateY(-4px)";
@@ -16,17 +14,7 @@ btnMenu.addEventListener('click', (e) => {
     e.stopPropagation();
 });
 
-// Fermer au clic extérieur
 document.addEventListener('click', () => {
     menuPrincipal.classList.remove('voir');
     barres.forEach(b => b.style.transform = "none");
-});
-
-// Parallaxe léger sur le Hero au scroll
-window.addEventListener('scroll', () => {
-    const hero = document.getElementById('hero');
-    let offset = window.pageYOffset;
-    if (window.innerWidth > 768) {
-        hero.style.backgroundPositionY = offset * 0.4 + "px";
-    }
 });
