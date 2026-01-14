fc28fbd2e7d9c698022fbe6f9683185bcebb97b8
Stanley Mazile <stanleymazilepro@gmail.com>
2026-01-14 04:38:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const btnMenu = document.getElementById('btnMenu');
 const liens = document.getElementById('liens-deroulants');
 
 btnMenu.addEventListener('click', () => {
-  btnMenu.classList.toggle('active');  // change le style du bouton
-  liens.style.display = liens.style.display === 'flex' ? 'none' : 'flex';
+  btnMenu.classList.toggle('active'); // change style du bouton
+  liens.style.display = liens.style.display === 'flex' ? 'none' : 'flex'; // toggle menu
 });
