191680ae5532a54eb3e6427c2bd4a28c57424157
Stanley Mazile <stanleymazilepro@gmail.com>
2026-01-14 04:19:04 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,7 @@
 const btnMenu = document.getElementById('btnMenu');
+const liens = document.getElementById('liens-deroulants');
 
-const menu = document.getElementById('liens-deroulants');
-
-
-
-btnMenu.onclick = function(e) {
-
-    menu.classList.toggle('voir');
-
-    e.stopPropagation();
-
-}
-
-
-
-window.onclick = function() {
-
-    menu.classList.remove('voir');
-
-}
-
-
-
-// Détecte le changement dans votre menu et ordonne la traduction intégrale
-
-document.getElementById('select-langue').addEventListener('change', function() {
-
-    var lang = this.value;
-
-    var googleCombo = document.querySelector('.goog-te-combo');
-
-    
-
-    if (googleCombo) {
-
-        googleCombo.value = lang;
-
-        googleCombo.dispatchEvent(new Event('change'));
-
-    }
-
+btnMenu.addEventListener('click', () => {
+  btnMenu.classList.toggle('active');  // change le style du bouton
+  liens.style.display = liens.style.display === 'flex' ? 'none' : 'flex';
 });
